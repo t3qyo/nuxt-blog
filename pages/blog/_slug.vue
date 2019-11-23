5501105af19009057b1aca71020cb23999e0fd12
sankyo.toshio <sankyo.toshio@tis.co.jp>
2019-11-23 17:02:48 +0900

    first commit

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="slug">
+    <h1 class="slug_title">タイトル</h1>
+    <p class="slug_date">2018/8/2</p>
+    <vue-markdown>
+# hogehoe title
+## fugafuga
+### ryuryu
+* aaa
+* bbb
+* ccc
+* ddd
+    </vue-markdown>
+  </section>
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown";
+export default {
+  transition: "slide-left",
+  components: {
+    VueMarkdown
+  }
+};
+</script>
+
+<style scoped>
+.slug {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.slug_title {
+  font-size: 2rem;
+  font-weight: bolder;
+}
+.slug_date {
+  font-size: 1rem;
+  color: rgb(57, 72, 85);
+  text-align: right;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="index">
+    <card v-for="i in 5" v-bind:key="i" />
+  </section>
+</template>
+
+<script>
+import Card from "~/components/card.vue";
+
+export default {
+  transition: "slide-left",
+  components: {
+    Card
+  }
+};
+</script>
+
+<style>
+.index {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
+
